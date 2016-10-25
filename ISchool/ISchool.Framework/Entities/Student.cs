71d55ceb183a1a115f6730a3d2dbc7bb4d9a2fdd
TheBigL <lmohamed1@studentmail.nait.ca>
2016-10-25 17:11:18 -0600

    Demonstration of .NET & SQL Power

---
+++ ISchool/ISchool.Framework/Entities/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISchool.Framework.Entities
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string CourseName { get; set; }
+    }
+
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("name=ISchoolDb") { }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+    }
+
+    [DataObject]
+    public class MyBLL
+    {
+        [DataObjectMethod(DataObjectMethodType.Select, true)]
+        {
+            using (var context = new SchoolContext())
+            {
+            return context.Student.ToList()
+            }
+        }
+    }
+
+
+}
